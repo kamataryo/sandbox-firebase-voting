d465ca5d0a95f1b37a634a8923aff7f7d97a516b
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2019-01-20 23:11:33 +0900

    Updates

---
+++ precache-manifest.4286b4eaa2e8121b8aa3e4fbc7a17e56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6753eb12b0dc6af8c796",
+    "url": "/sandbox-firebase-voting/static/css/main.956b49c7.chunk.css"
+  },
+  {
+    "revision": "6753eb12b0dc6af8c796",
+    "url": "/sandbox-firebase-voting/static/js/main.6753eb12.chunk.js"
+  },
+  {
+    "revision": "12e5135d00acda698cb2",
+    "url": "/sandbox-firebase-voting/static/js/1.12e5135d.chunk.js"
+  },
+  {
+    "revision": "dc606c3ce6009e407219",
+    "url": "/sandbox-firebase-voting/static/js/runtime~main.dc606c3c.js"
+  },
+  {
+    "revision": "497e4be7d3faba0040028492c5ea8fc1",
+    "url": "/sandbox-firebase-voting/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/sandbox-firebase-voting/precache-manifest.4286b4eaa2e8121b8aa3e4fbc7a17e56.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/sandbox-firebase-voting/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
