1d4cf0d859da4cebb7c8db905be9626f42190c40
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2019-01-20 23:30:18 +0900

    Updates

--- precache-manifest.393a1c4f881d262ac56d9dface2934c2.js
+++ precache-manifest.393a1c4f881d262ac56d9dface2934c2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "2e5d70221afa7a3929e5",
+    "revision": "510bca2f94234d9810a9",
     "url": "/sandbox-firebase-voting/static/css/main.5e9d22df.chunk.css"
   },
   {
-    "revision": "2e5d70221afa7a3929e5",
-    "url": "/sandbox-firebase-voting/static/js/main.2e5d7022.chunk.js"
+    "revision": "510bca2f94234d9810a9",
+    "url": "/sandbox-firebase-voting/static/js/main.510bca2f.chunk.js"
   },
   {
     "revision": "12e5135d00acda698cb2",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/sandbox-firebase-voting/static/js/runtime~main.dc606c3c.js"
   },
   {
-    "revision": "4298d8521eb0da6729bc92fc4202bd53",
+    "revision": "73b58bd357355e36c6751d4f2aaadeeb",
     "url": "/sandbox-firebase-voting/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sandbox-firebase-voting/precache-manifest.958cf75047fb1bf6254f93d8b968bd82.js"
+  "/sandbox-firebase-voting/precache-manifest.393a1c4f881d262ac56d9dface2934c2.js"
 );
 
 workbox.clientsClaim();
